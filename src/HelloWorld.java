8224dc10c0fe6ca020eeb7084e88141d301f7bff
Cezary Slotwinski <seezar@o2.pl>
2018-07-02 11:12:07 +0200

    Dodano nowa linie

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Czesc! Dziala!");
+        System.out.println("Bardzo fajny program");
+
+    }
+}
